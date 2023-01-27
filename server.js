f3dcff6d4300776570b7a610b467a975fbf2ea7d
Kristine Kochie <kristinekochie@gmail.com>
2023-01-26 20:04:25 -0500

    Inital commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const connectDB = require("./config/db")
+const logger = require("./utils/logger")
+
+dotenv.config({ path: "./config/config.env" })
+
+connectDB()
+
+const app = express()
+
+const PORT = process.env.PORT || 5001
+
+app.use(logger)
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`)
+})
+
+process.on("unhandledrejection", (err) => {
+  console.log(`Error: ${err.message}`)
+  server.close(() => process.exit(1))
+})
